4fd99b881fc05a7c24573ee97f834f7939a17fe9
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2018-12-06 21:31:20 +0800

    update

--- nlsr_configs/generate.sh
+++ nlsr_configs/generate.sh
@@ -4,8 +4,8 @@ path=$2
 
 routerName=$(echo ${name#/ndn/edu/pkusz})
 cat /dev/null > nlsr.conf
-$PATH/nlsr_configs/generate-general.sh /ndn /edu/pkusz $routerName >> nlsr.conf
-$PATH/nlsr_configs/generate-neighbour.sh $name $path >> nlsr.conf
-$PATH/nlsr_configs/generate-hyperbolic.sh >> nlsr.conf
-$PATH/nlsr_configs/generate-fib.sh >> nlsr.conf
-$PATH/nlsr_configs/generate-security.sh >> nlsr.conf
+$path/nlsr_configs/generate-general.sh /ndn /edu/pkusz $routerName >> nlsr.conf
+$path/nlsr_configs/generate-neighbour.sh $name $path >> nlsr.conf
+$path/nlsr_configs/generate-hyperbolic.sh >> nlsr.conf
+$path/nlsr_configs/generate-fib.sh >> nlsr.conf
+$path/nlsr_configs/generate-security.sh >> nlsr.conf
